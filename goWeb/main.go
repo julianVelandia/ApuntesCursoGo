354924dadce3eacbee918f5f015b0f1e433bd079
julianVelandia <jvelandiag@unal.edu.co>
2021-06-28 23:22:33 -0500

    .

--- goWeb/main.go
+++ goWeb/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -11,12 +12,14 @@ func main() {
 		//w.Header().Add("nombre", "valor")
 		//fmt.Println("El método es: " + r.Method)
 
-		fmt.Fprintf(w, "Hola mundo")
-		//http.Redirect(w, r, "/adios", http.StatusMovedPermanently)
+		//fmt.Fprintf(w, "Hola mundo")
+		http.Redirect(w, r, "/redireccion", http.StatusMovedPermanently)
 
 	})
 
-	http.HandleFunc("/adios", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/redireccion", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Redireccionamiento\n \n")
+
 		switch r.Method {
 		case "GET":
 			fmt.Fprintf(w, "El método es GET")
@@ -28,8 +31,6 @@ func main() {
 			fmt.Fprintf(w, "El método es DELETE")
 		}
 
-		fmt.Fprintf(w, "\nHola mundo")
-
 		//Para trabajar con la url se usa r
 
 		fmt.Println(r.URL.Query())
@@ -38,12 +39,58 @@ func main() {
 			fmt.Println(parametro)
 		}
 
-		//http.Redirect(w, r, "/adios", http.StatusMovedPermanently)
+		//Generar query
+		fmt.Println(r.URL)
+		values := r.URL.Query()
+		values.Add("nombre", "Julián")
+		values.Add("edad", "21")
+		values.Add("profesión", "ingeniero")
 
+		r.URL.RawQuery = values.Encode()
+		fmt.Println(r.URL)
 		//http.NotFound(w, r)
 		//http.Error(w, "primer error", http.StatusConflict)
+
+		//Headers
+		fmt.Println(r.Header)
+
+		//crear urls
+
+		url := createURL()
+		fmt.Println("la url final es:", url)
+
+		//Generar peticiones
+		request, err := http.NewRequest("GET", url, nil)
+
+		if err != nil {
+			panic(err)
+		}
+		cliente := &http.Client{}
+		response, err := cliente.Do(request)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("header: ", response.Header)
+		fmt.Println("body", response.Body)
+		fmt.Println("status", response.Status)
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 
+}
+
+func createURL() string {
+	u, err := url.Parse("/params")
+	if err != nil {
+		panic(err)
+	}
+
+	u.Host = "localhost:300"
+	u.Scheme = "http"
+
+	query := u.Query() //map
+	query.Add("nombre", "valor")
+	u.RawQuery = query.Encode()
+	return u.String()
 }
